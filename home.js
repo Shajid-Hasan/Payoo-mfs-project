87eb23cd028578d0dc415d682a221f9c95ba2fc3
Shajid <hasan.shajid222@gmail.com>
2025-08-25 02:13:45 +0600

    added new

--- home.js
+++ home.js
@@ -31,20 +31,15 @@ document.getElementById('add-money').addEventListener('click', function(event){
 
     document.getElementById('available-balance').
     innerText = totalNewAvailableBalance;
+})
 
+// Taggling features
 
-
-    // const bank = document.getElementById('bank').value
-    // const accountNumber = document.getElementById('account-number').value
-    // const amount = parseInt(document.getElementById('add-amount').value)
-
-    // const pin = document.getElementById('add-pin').value
-
-    // const availableBalance = parseInt(document.getElementById('available-balance').innerText)
-    // console.log(availableBalance)
-
-    // const totalNewAvailableBalance = amount + availableBalance;
-    
-    // document.getElementById('available-balance').
-    // innerText = totalNewAvailableBalance
+document.getElementById('add-btn').addEventListener('click', function(){
+    document.getElementById('cash-out-parent').style.display = 'none';
+    document.getElementById('add-money-parent').style.display = 'block';
+})
+document.getElementById('cash-out-btn').addEventListener('click', function(){
+    document.getElementById('add-money-parent').style.display = 'none';
+    document.getElementById('cash-out-parent').style.display = 'block';
 })
